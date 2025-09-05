034c892326121e4f9ee382d2c04ebb7bbae5bab5
Leia Lai <leilai@c2r8s7.42lausanne.ch>
2025-09-05 14:34:33 +0200

    some more malloc

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leilai <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 12:26:11 by leilai            #+#    #+#             */
+/*   Updated: 2025/09/05 12:28:51 by leilai           ###   ####lausanne.ch   */
+/*                                                                            */
+/* ************************************************************************** */
+
+//int **range - a pointer to a pointer to int
+//*range - a pointer to int (what range points to)
+//**range - an int (the actual value)
+
+#include <stdlib.h>
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	*array;
+	int	size;
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	size = max - min;
+	array = malloc(sizeof(int) * size);
+	if (!array)
+	{
+		*range = NULL;
+		return (-1);
+	}
+	//logic
+	i = 0;
+	while (i < size)
+	{
+		array[i] = min + i;
+		i++;
+	}
+	*range = array;
+	return (size);
+}
---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leilai <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 12:37:07 by leilai            #+#    #+#             */
+/*   Updated: 2025/09/05 14:31:50 by leilai           ###   ####lausanne.ch   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *s);
+int	result_len(int size, char **strs, char *sep);
+char	*ft_strcat(char *dest, char *src);
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*result;
+	int	len;
+	int	i;
+
+	if (size == 0)
+	{
+		result = malloc(1);
+		result = NULL;
+		return (result);
+	}
+	len = result_len(size, strs, sep);
+	result = malloc(len);
+	if (!result)
+		return (result);
+	result[0] = '\0';
+	i = 0;
+	while (i < size)
+	{
+		result = ft_strcat(result, strs[i]);
+		if (i + 1 < size)
+			result = ft_strcat(result, sep);
+		i++;
+	}
+	result[len] = '\0';
+	return (result);
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(dest);
+	while (src[i])
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
+
+int	result_len(int size, char **strs, char *sep)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			j++;
+			len++;
+		}
+		i++;
+	}
+	len = len + ft_strlen(sep) * (size - 1) + 1;
+	return (len + 1);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char	str[] = "Hello";
+	char	str1[] = "World";
+	char	str2[] = "!";
+	char	*str012[] = {str, str1, str2};
+	char	*result = ft_strjoin(3, str012, "-");
+
+	printf("%s", result);
+	free(result);
+	return (0);
+}
+
